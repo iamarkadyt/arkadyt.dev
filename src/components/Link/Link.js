ae1b58fcbae76d237725df40bcd80e941f0d7257
iamarkadyt <pgp-noreply@rkd.dev>
2018-05-25 21:44:01 -0700

    Made <Link/> stateless.

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -2,17 +2,13 @@ import React from 'react';
 import styles from './Link.css';
 import { GoChevronRight } from 'react-icons/lib/go';
 
-class Link extends React.Component {
-    render() {
-        return (
-            <a style={styles.a} href={this.props.url}>
-                <div style={styles.wrapper}>
-                    <span style={styles.text}>{this.props.title}</span>
-                    <GoChevronRight style={styles.arrow} />
-                </div>
-            </a>
-        );
-    }
+function link(props) {
+    return <a style={styles.a} href={props.url}>
+        <div style={styles.wrapper}>
+            <span style={styles.text}>{props.title}</span>
+            <GoChevronRight style={styles.arrow} />
+        </div>
+    </a>;
 }
 
-export default Link;
+export default link;
