93dd6000ad2e4155f9eb5e67dacab177707e9cdc
iamarkadyt <pgp-noreply@rkd.dev>
2018-06-01 19:24:20 -0700

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
 
-import List from './components/List/List';
-import Footer from './components/Footer/Footer';
-import EmailMe from './components/EmailMe/EmailMe';
-import Background from './components/Background/Background';
-import NavBar from './components/NavBar/NavBar';
-import AnimatedName from './components/AnimatedName/AnimatedName';
-
-import dummyData from './misc/dummyData';
-
-class App extends Component {
-  state = {
-    projects: dummyData,
-    references: new Array(3).fill().map(() => {
-      return React.createRef()
-    })
-  }
-
-  render() {
-    return <div>
-      <Background />
-
-      <NavBar refsList={this.state.references} />
-      <AnimatedName />
-
-      <div style={{ height: '150vh' }} ref={this.state.references[0]}></div>
-      <List ref={this.state.references[1]} projectsList={this.state.projects} />
-      <EmailMe ref={this.state.references[2]} />
-
-      <Footer />
-    </div>;
-  }
+class App extends React.Component {
+    render() {
+        return '';
+    }
 }
 
 export default App;
--- src/misc/colorPalette.js
+++
@@ -1,6 +0,0 @@
-export default {
-    COLOR_LIGHT: '#555',
-    COLOR_DARK: '#333',
-    FONT_WHITE: '#eee',
-    _HIGHLIGHTED: '#ffa500',
-} 
