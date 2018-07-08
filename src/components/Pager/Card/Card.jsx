1a189408030758212397f36ef147edfd7d507299
iamarkadyt <pgp-noreply@rkd.dev>
2018-07-07 18:11:18 -0700

    Set CSS grid at Card.jsx

--- src/components/Pager/Card/Card.jsx
+++ src/components/Pager/Card/Card.jsx
@@ -5,14 +5,16 @@ import classes from './Card.module.css';
 const card = props => {
     return <div className={classes.container}>
         <img src={props.img} alt='' className={classes.background} />
-        <img src={props.img} alt='' className={classes.widget} />
-        <div className={classes.description}>
-            <h1 className={classes.header}>{props.header}</h1><br />
-            <p>{props.body}</p><br />
-            {props.tags.map((item, index) => {
-                return <span key={index} className={classes.tag}>{item}</span>;
-            })}<br />
-            <a href={props.url}>View on GitHub</a>
+        <div className={classes.grid}>
+            <img src={props.img} alt='' className={classes.widget} />
+            <div className={classes.description}>
+                <h1 className={classes.header}>{props.header}</h1><br />
+                <p>{props.body}</p><br />
+                {props.tags.map((item, index) => {
+                    return <span key={index} className={classes.tag}>{item}</span>;
+                })}<br />
+                <a href={props.url}>View on GitHub</a>
+            </div>
         </div>
     </div>;
 };
