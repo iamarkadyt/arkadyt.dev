048a5963557f6640018c4fa6abab0a37986d02bf
iamarkadyt <pgp-noreply@rkd.dev>
2018-07-08 19:45:16 -0700

    Update projects media.

--- src/projects.content.js
+++ src/projects.content.js
@@ -2,28 +2,28 @@ export default [
     {
         header: 'Demonstration of Physics Laws',
         url: 'http://github.com/arkadyt/phys-bund',
-        images: ['https://picsum.photos/1920/1080/?random'],
+        images: ['https://i.imgur.com/y5Wb6eM.jpg', 'https://i.imgur.com/b7ghmIi.jpg', 'https://imgur.com/3GiMYlm.jpg', 'https://imgur.com/OzxoiGn.jpg', 'https://imgur.com/whnoQ5U.jpg'],
         body: 'Bundle of Demonstrations of Physics Phenomena. Has been developed in team of me and my classmate during our highschool physics curriculum renovation.',
         tags: ['Unity', 'C#', '3D Studio Max'],
     },
     {
-        header: 'UP!',
-        url: 'http://github.com/arkadyt/UP',
-        images: ['https://picsum.photos/1920/1080/?random', 'https://picsum.photos/1920/1079/?random'],
-        body: 'Mobile game which objective is to fly as high as you can whilst avoiding rapidly moving obstacles.',
-        tags: ['Unity', 'C#', 'Adobe Photoshop'],
-    },
-    {
         header: 'zZloBot',
         url: 'http://github.com/arkadyt/zzlobot',
-        images: ['https://picsum.photos/1920/1078/?random', 'https://picsum.photos/1920/1077/?random', 'https://picsum.photos/1920/1076/?random'],
+        images: ['https://i.imgur.com/AtYq4A7.jpg', 'https://i.imgur.com/RH27HFn.jpg', 'https://i.imgur.com/LHXYMbJ.jpg'],
         body: 'A bot for a mobile game \'Epic Might\'. Has been developed to automate all sorts of routine in-game tasks. Has been adopted by hundreds of players across the globe.',
         tags: ['Android', 'Java', 'HSL'],
     },
     {
+        header: 'UP!',
+        url: 'http://github.com/arkadyt/UP',
+        images: ['https://placehold.it/1920/1080'],
+        body: 'Mobile game which objective is to fly as high as you can whilst avoiding rapidly moving obstacles.',
+        tags: ['Unity', 'C#', 'Adobe Photoshop'],
+    },
+    {
         header: 'Reddit Activity Monitor',
         url: 'http://github.com/arkadyt/_UCSX_Reddit_Monitor',
-        images: ['https://picsum.photos/1920/1075/?random', 'https://picsum.photos/1920/1074/?random', 'https://picsum.photos/1920/1073/?random', 'https://picsum.photos/1920/1072/?random'],
+        images: ['https://placehold.it/1920/1080'],
         body: 'Solution written in JavaScript that collects information from selected subreddits and uses Plot.ly API to create graphical representations of this data. Program successfully solved the problem of misunderstanding of user activity patterns.',
         tags: ['JavaScript'],
     }
