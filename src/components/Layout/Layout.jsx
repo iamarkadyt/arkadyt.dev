9798fbceba1c3188dfc212c25e235737bddd9198
iamarkadyt <pgp-noreply@rkd.dev>
2018-07-16 21:27:37 -0700

    Move Layout into its own dir. Add background image.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import NavBar from './NavBar/NavBar';
-import Section from './Section/Section';
+import NavBar from '../NavBar/NavBar';
+import Section from '../Section/Section';
 import './Layout.css';
 
 import IconGitHub from 'react-icons/lib/go/mark-github';
@@ -10,12 +10,13 @@ import IconDOCX from 'react-icons/lib/fa/file-word-o';
 export default class Layout extends Component {
     render() {
         const testLinks = [
-            { title: <IconGitHub/>, href: 'https://a.com/1' },
-            { title: <IconLinkedIn/>, href: 'https://a.com/2' },
-            { title: <IconDOCX/>, href: 'https://a.com/3' }
+            { title: <IconGitHub />, href: 'https://a.com/1' },
+            { title: <IconLinkedIn />, href: 'https://a.com/2' },
+            { title: <IconDOCX />, href: 'https://a.com/3' }
         ];
 
         return <React.Fragment>
+            <div className="Layout-content-background" />
             <NavBar links={testLinks} />
             <div className="Layout-content">
                 <Section title="Personal Projects"></Section>
