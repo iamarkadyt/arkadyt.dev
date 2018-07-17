9a9e0c6585d394b6f2e8b55e5b780fa71927c818
iamarkadyt <pgp-noreply@rkd.dev>
2018-07-16 18:36:20 -0700

    Pre-start cleanup.

--- src/utils/utils.js
+++
@@ -1,6 +0,0 @@
-export const sentenceToURL = sentence => {
-    let result = sentence;
-    result = result.replace(/\s/g, '_');
-    result = result.replace(/\W/g, '');
-    return result.toLowerCase();
-}
