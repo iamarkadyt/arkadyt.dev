2e206d15373e83c5d60504ca1b47b4b76b74c026
iamarkadyt <pgp-noreply@rkd.dev>
2018-07-24 11:29:09 -0700

    Add external links support to Card component.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Card.css';
+import IconHref from 'react-icons/lib/fa/external-link';
 
 const card = props => {
     return <div className="Card-container">
@@ -13,6 +14,21 @@ const card = props => {
                 {props.tags.map((item, index) => (
                     <span key={index}>{item}</span>
                 ))}
+            </div>
+            <div className="links">
+                {props.links.map((item, index) => {
+                    let className, title;
+                    if (item.includes('youtu'))
+                        className = title = 'YouTube';
+                    else if (item.includes('github'))
+                        className = title = 'GitHub';
+                    else className = title = 'Link';
+                    return <a key={index} href={item} target="_blank">
+                        <span className={className}>
+                            {title}&nbsp;&nbsp;<IconHref />
+                        </span>
+                    </a>
+                })}
             </div>
         </div>
     </div>;
