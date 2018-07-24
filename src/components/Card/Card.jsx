967a0c2a19321cb0b408bb08ec5584a55b49622d
iamarkadyt <pgp-noreply@rkd.dev>
2018-07-24 15:46:30 -0700

    Add Card flip animation.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,32 +4,32 @@ import IconHref from 'react-icons/lib/fa/external-link';
 
 const card = props => {
     return <div className="Card-container">
-        <a href={props.href} target="_blank">
+        <a href={props.href} target="_blank" className="Card-container">
             <img src={props.image} alt='' />
+            <div className="description">
+                <h3>{props.title}</h3>
+                <span>{props.description}</span>
+                <div className="tags">
+                    {props.tags.map((item, index) => (
+                        <span key={index}>{item}</span>
+                    ))}
+                </div>
+            </div>
         </a>
-        <div className="description">
-            <h3>{props.title}</h3>
-            <span>{props.description}</span>
-            <div className="tags">
-                {props.tags.map((item, index) => (
-                    <span key={index}>{item}</span>
-                ))}
-            </div>
-            <div className="links">
-                {props.links.map((item, index) => {
-                    let className, title;
-                    if (item.includes('youtu'))
-                        className = title = 'YouTube';
-                    else if (item.includes('github'))
-                        className = title = 'GitHub';
-                    else className = title = 'Link';
-                    return <a key={index} href={item} target="_blank">
-                        <span className={className}>
-                            {title}&nbsp;&nbsp;<IconHref />
-                        </span>
-                    </a>
-                })}
-            </div>
+        <div className="backface">
+            {props.links.map((item, index) => {
+                let className, title;
+                if (item.includes('youtu'))
+                    className = title = 'YouTube';
+                else if (item.includes('github'))
+                    className = title = 'GitHub';
+                else className = title = 'Link';
+                return <a key={index} href={item} target="_blank">
+                    <span className={className}>
+                        {title}&nbsp;&nbsp;<IconHref />
+                    </span>
+                </a>
+            })}
         </div>
     </div>;
 };
