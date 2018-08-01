83a4f1f2915db843defcd053c50e4cc5aa487598
iamarkadyt <pgp-noreply@rkd.dev>
2018-07-31 20:20:01 -0700

    Set ID prop for all sections.

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Section.css';
 
 const section = props => {
-    return <div className={`Section-container ${props.blue ? 'Section-blue' : ''}`}>
+    return <div id={props.id} className={`Section-container ${props.blue ? 'Section-blue' : ''}`}>
         <div className="content">
             <h1>{props.title}</h1>
             {props.children}
