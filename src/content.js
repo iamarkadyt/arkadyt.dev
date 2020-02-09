d0bac99d2cbfdb5f47a24a57088848acd99a0416
iamarkadyt <pgp-noreply@rkd.dev>
2020-02-09 10:37:05 -0800

    Remove non-webdev projects, upd content

--- src/content.js
+++ src/content.js
@@ -20,20 +20,20 @@ export const links = [
 
 export const jobs = [
     {
-        company: 'Full Stack Software Engineer',
-        title: 'HP Inc.',
-        date: 'April 2019 - Present (10 mos)',
+        company: 'Full-Stack Software Engineer',
+        title: 'Hewlett-Packard Inc',
+        date: 'April 2019 - Present (1 year)',
         image: ImgHP,
-        imgWidth: 18
+        imgWidth: 40
     }
 ]
 
 export const projects = [
     {
-        href: 'https://github.com/arkadyt/ww.net',
+        href: 'https://github.com/arkadyt/wework',
         image: 'https://i.imgur.com/nCAXDkVh.png',
-        title: 'WW.NET',
-        description: 'Fully-fledged social networking platform built with JavaScript, React, Redux, Node.js, Express and others. Not a full Twitter copy, but a very close one!',
+        title: 'WeWork',
+        description: 'Fully-fledged social networking platform built with JavaScript, React, Redux, Node.js, Express and others. Not a full Twitter copy, but a close one!',
         tags: ['JavaScript', 'React', 'Redux', 'Node', 'Express', 'MongoDB', 'AWS'],
         links: [
             { title: 'Website', href: 'http://socnet.arkadyt.com' },
@@ -41,36 +41,14 @@ export const projects = [
         ],
     },
     {
-        href: 'https://github.com/arkadyt/phys-bund',
-        image: 'https://i.imgur.com/y5Wb6eMh.jpg',
-        title: 'Demonstrations of Physics Phenomena',
-        description: 'Educational desktop application intended to support high school physics curriculum with engaging 3D visuals and interactive virtual equipment.',
-        tags: ['C#', '3DSMax', 'Unity3D'],
+        href: 'https://github.com/arkadyt/vspace',
+        image: 'https://i.imgur.com/nCAXDkVh.png',
+        title: 'vSpace',
+        description: 'Fully-fledged eCommerce platform. Built with JavaScript, React, Redux, Node.js, Express and others.',
+        tags: ['JavaScript', 'React', 'Redux', 'Node', 'Express', 'MongoDB', 'AWS'],
         links: [
-            { title: 'Video', href: 'https://youtu.be/0eX7hdSTzko' },
-            { title: 'GitHub', href: 'https://github.com/arkadyt/phys-bund' },
-        ],
-    },
-    {
-        href: 'https://github.com/arkadyt/zzlobot',
-        image: 'https://i.imgur.com/v0x4PYnh.jpg',
-        title: 'zZloBot',
-        description: 'Mobile game bot that allows players to automate over 200 different quests and tasks. Built for Android with Java and HMSL.',
-        tags: ['Java', 'Android', 'HMSL'],
-        links: [
-            { title: 'Video', href: 'https://youtu.be/oR2P2E2VgeE' },
-            { title: 'GitHub', href: 'https://github.com/arkadyt/zzlobot' },
-        ],
-    },
-    {
-        href: 'https://github.com/arkadyt/up.game',
-        image: 'https://i.imgur.com/mCFO8rMh.jpg',
-        title: 'UP!',
-        description: 'Multiplatform mobile game made with Unity3D, Photoshop and C#. It\'s quite challenging actually! =)',
-        tags: ['C#', 'Photoshop', 'Unity3D'],
-        links: [
-            { title: 'Video', href: 'https://youtu.be/9axXzYZFRkQ' },
-            { title: 'GitHub', href: 'https://github.com/arkadyt/up.game' },
+            { title: 'Website', href: 'http://socnet.arkadyt.com' },
+            { title: 'GitHub', href: 'https://github.com/arkadyt/ww.net' },
         ],
     },
 ]
