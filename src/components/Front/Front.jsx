d507048aeb9fe6e5fe17ede1f605e90d8e78cf44
iamarkadyt <pgp-noreply@rkd.dev>
2020-02-12 09:55:25 -0800

    Update title

--- src/components/Front/Front.jsx
+++ src/components/Front/Front.jsx
@@ -5,8 +5,8 @@ const front = () => {
     return <div className="Front-container">
         <div className="name-block">
             <h1>Arkady</h1><h3>Titenko</h3><br />
-            <span>Full-Stack React.JS Developer</span>
-            <span>Level Designer</span>
+            <span>Software Developer</span>
+            <span>Cloud Solutions Architect</span>
         </div>
     </div>;
 };
