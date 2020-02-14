f99805accaffd8b766e350100ed5503615082ffa
iamarkadyt <pgp-noreply@rkd.dev>
2020-02-13 23:42:49 -0800

    Restructure again, add todo list

--- src/content/skills.js
+++ src/content/skills.js
@@ -1,8 +1,31 @@
-import ImgReact from '../images/react.svg';
+import ImgReact from '../images/skills/react.svg';
+import ImgNode from '../images/skills/node2.svg';
+import ImgJS from '../images/skills/js.svg';
+import ImgHTML from '../images/skills/html5.svg';
+import ImgCSS from '../images/skills/css3.svg';
+import Java from '../images/skills/java.svg';
+import Python from '../images/skills/python.svg';
 
 export default [
   {
     icon: ImgReact,
+    title: 'React.js',
+    imgWidth: 18,
+  },
+  {
+    icon: ImgNode,
     title: 'React.js'
-  }
+  },
+  {
+    icon: ImgJS,
+    title: 'React.js'
+  },
+  {
+    icon: ImgHTML,
+    title: 'React.js'
+  },
+  {
+    icon: ImgCSS,
+    title: 'React.js'
+  },
 ];
