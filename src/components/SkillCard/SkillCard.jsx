a427dbf6470669af29af2d55b8bdecf20622ed3e
iamarkadyt <pgp-noreply@rkd.dev>
2020-02-15 12:25:03 -0800

    Add more skill cards, more bug fixes

--- src/components/SkillCard/SkillCard.jsx
+++ src/components/SkillCard/SkillCard.jsx
@@ -3,15 +3,15 @@ import Tooltip from '../Tooltip/Tooltip.jsx';
 import './SkillCard.css';
 
 const SkillCard = props => {
-  const { icon, title, wide } = props;
+  const { icon, title, wide, megaWide, ultraWide, height } = props;
   const classes = [
     "SkillCard-container",
-    wide ? "two-cell" : ""
+    wide ? "two-cell" : megaWide ? "three-cell" : ultraWide ? "four-cell" : "",
   ];
 
   return (
     <div className={classes.join(" ")}>
-      <img alt='' src={icon} />
+      <img alt='' src={icon} style={{ height: `${height}%` }} />
       <Tooltip text={title} />
     </div>
   );
--- src/content/skills.js
+++ src/content/skills.js
@@ -1,32 +1,137 @@
-import ImgReact from '../images/skills/react.svg';
-import ImgNode from '../images/skills/node2.svg';
-import ImgJS from '../images/skills/js.svg';
-import ImgHTML from '../images/skills/html5.svg';
-import ImgCSS from '../images/skills/css3.svg';
+import React from '../images/skills/reactbig.svg';
+import NodeJS from '../images/skills/node2.svg';
+import JS from '../images/skills/js.svg';
+import HTML from '../images/skills/html5.svg';
+import CSS from '../images/skills/css3.svg';
+
+import Docker from '../images/skills/docker.svg';
+import MongoDB from '../images/skills/mongodb.svg';
+
+import AWS from '../images/skills/aws.svg';
+import GraphQL from '../images/skills/graphql.svg';
+import Webpack from '../images/skills/webpack.svg';
+import Redis from '../images/skills/redis.svg';
+import PWA from '../images/skills/pwa.svg';
+
+import Git from '../images/skills/git.svg';
+import Redux from '../images/skills/redux.svg';
+import Sass from '../images/skills/sass.svg';
+import Less from '../images/skills/less.svg';
 import Java from '../images/skills/java.svg';
 import Python from '../images/skills/python.svg';
+import Kubernetes from '../images/skills/kubernetes.svg';
+import Apollo from '../images/skills/apollo.svg';
+
+import Linux from '../images/skills/linux.svg';
+import Bash from '../images/skills/bash.svg';
+import TravisCI from '../images/skills/travisci.svg';
+import CircleCI from '../images/skills/circleci.svg';
+import GulpJS from '../images/skills/gulp.svg';
+
+const emDash = '—';
 
 export default [
   {
-    icon: ImgReact,
+    icon: React,
     title: 'React.js',
-    imgWidth: 18,
+    megaWide: true
   },
   {
-    icon: ImgNode,
+    icon: NodeJS,
     title: 'NodeJS',
     wide: true,
   },
   {
-    icon: ImgJS,
-    title: 'React.js'
+    icon: HTML,
+    title: 'HTML'
   },
   {
-    icon: ImgHTML,
-    title: 'React.js'
+    icon: CSS,
+    title: 'CSS'
   },
   {
-    icon: ImgCSS,
-    title: 'React.js'
+    icon: JS,
+    title: 'JavaScript'
+  },
+  {
+    icon: Java,
+    title: 'Java'
+  },
+  {
+    icon: Python,
+    title: 'Python'
+  },
+  {
+    icon: AWS,
+    title: `AWS Cloud`,
+    megaWide: true
+  },
+  {
+    icon: Docker,
+    title: 'Docker'
+  },
+  {
+    icon: Kubernetes,
+    title: 'Kubernetes'
+  },
+  {
+    icon: MongoDB,
+    title: 'MongoDB',
+    ultraWide: true
+  },
+  {
+    icon: Redux,
+    title: 'Redux.js'
+  },
+  {
+    icon: PWA,
+    title: `Progressive Web Apps`,
+    megaWide: true,
+    height: 80,
+  },
+  {
+    icon: Webpack,
+    title: 'Webpack'
+  },
+  {
+    icon: GraphQL,
+    title: 'GraphQL'
+  },
+  {
+    icon: Apollo,
+    title: 'Apollo'
+  },
+  {
+    icon: Git,
+    title: 'Git',
+    megaWide: true
+  },
+  {
+    icon: Redis,
+    title: 'Redis',
+  },
+  {
+    icon: Sass,
+    title: `SASS / SCSS`,
+    wide: true
+  },
+  {
+    icon: Less,
+    title: `LESS`,
+    wide: true,
+    height: 88,
+  },
+  {
+    icon: GulpJS,
+    title: "Gulp.js"
+  },
+  {
+    icon: Bash,
+    title: "Bash",
+    megaWide: true
+  },
+  {
+    icon: Linux,
+    title: "Linux Systems\nAdministration"
   },
 ];
