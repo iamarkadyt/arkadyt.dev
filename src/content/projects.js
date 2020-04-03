459a9fdc495804e396c5dce679235d0a56f6de68
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-02 22:28:44 -0700

    Use absolute imports for project imgs

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
-// TODO use firebase cdn instead of imgur.
-// do so by storing images in this project
-// then deploying them to firebase.
+import weworkWEBP from 'images/projects/wework.webp';
+import weworkJPG from 'images/projects/wework.jpg';
+import vspaceWEBP from 'images/projects/vspace.webp';
+import vspaceJPG from 'images/projects/vspace.jpg';
 
 export default [
     {
         href: 'https://github.com/arkadyt/wework',
-        image: 'https://i.imgur.com/nCAXDkVh.png',
+        image: {
+            src: weworkWEBP,
+            fallback: weworkJPG,
+        },
         title: 'WeWork',
         description: 'Fully-fledged social networking platform built with JavaScript, React, Redux, Node.js, Express and others. Not a full Twitter copy, but a close one!',
         tags: ['JavaScript', 'React', 'Redux', 'Node', 'Express', 'MongoDB', 'AWS'],
@@ -18,7 +21,10 @@ export default [
     },
     {
         href: 'https://github.com/arkadyt/vspace',
-        image: 'https://i.imgur.com/nCAXDkVh.png',
+        image: {
+            webp: vspaceWEBP,
+            fallback: vspaceJPG,
+        },
         title: 'vSpace',
         description: 'Fully-fledged eCommerce platform. Built with JavaScript, React, Redux, Node.js, Express and others.',
         tags: ['JavaScript', 'React', 'Redux', 'Node', 'Express', 'MongoDB', 'AWS'],
