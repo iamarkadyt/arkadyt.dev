2900f2453f418c1204a39dba0ba90aa8d87863a0
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-18 22:50:31 -0700

    Calculate job longevity automatically

--- src/components/cards/experience/index.jsx
+++ src/components/cards/experience/index.jsx
@@ -4,9 +4,35 @@ import BigButton from 'components/shared/bigbutton';
 import clsx from 'clsx';
 import './styles.scss';
 
-/**
- * Work experience card.
- */
+const ensurePlural = (count, singularForm) => {
+    if (count > 1) return `${count} ${singularForm}s`;
+    else return `${count} ${singularForm}`;
+}
+
+const appendLongevity = str => {
+    try {
+        const split = str.split('—').map(el => el.trim());
+        split[0] = new Date(split[0]);
+
+        if (split[1].toLowerCase() === "present") {
+            split[1] = new Date();
+        } else {
+            split[1] = new Date(split[1]);
+        }
+
+        let longevity = Math.round((split[1] - split[0]) / 1000 / 60 / 60 / 24 / 30);
+        if (longevity > 12) {
+            let years = Math.floor(longevity / 12);
+            return `${str} (${ensurePlural(years, 'yr')}. and ${ensurePlural(longevity % 12, 'mo')}.)`;
+        } else {
+            return `${str} (${ensurePlural(longevity, 'mo')}.)`;
+        }
+    } catch (e) {
+        console.error('Could not parse date string!', e);
+        return str;
+    }
+};
+
 const WexpCard = props => {
     const { imgWidth, date, company, title, number } = props;
     const { theme } = useContext(ThemeContext);
@@ -26,7 +52,7 @@ const WexpCard = props => {
                      style={{ width: `${imgWidth}%`, color: 'white' }} 
                      alt='' />
                 <div className="text">
-                    <h4>{date}</h4>
+                    <h4>{appendLongevity(date)}</h4>
                     <h2>{company}</h2>
                     <h3>{title}</h3>
                 </div>
