751b731fd56e00acc90fa933580820c55a246ada
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-19 13:44:46 -0700

    Set last and first child margins

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -4,13 +4,12 @@ import clsx from 'clsx';
 import './styles.scss';
 
 import NavBar from 'components/navbar';
-import Intro from 'components/blocks/intro';
+import Header from 'components/blocks/header';
 import AboutMe from 'components/blocks/about';
 import Experience from 'components/blocks/experience';
 import Skills from 'components/blocks/skills';
 import Projects from 'components/blocks/projects';
 import Publications from 'components/blocks/publications';
-import Contact from 'components/blocks/contact';
 import Footer from 'components/blocks/footer';
 
 const Layout = props => {
@@ -19,13 +18,14 @@ const Layout = props => {
         <Fragment>
             <NavBar />
             <div className={clsx("Layout-content", theme)}>
-                <Intro />
-                <AboutMe />
-                <Experience />
-                <Skills />
-                <Projects />
-                <Publications />
-                <Contact />
+                <Header />
+                <div className="content-body">
+                    <AboutMe />
+                    <Experience />
+                    <Skills />
+                    <Projects />
+                    <Publications />
+                </div>
                 <Footer />
             </div>
         </Fragment>
