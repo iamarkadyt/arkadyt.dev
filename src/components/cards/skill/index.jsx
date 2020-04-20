3355005b0e4bc29612dc59857f38662fbe695ef4
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-19 19:50:28 -0700

    Fix skillcard issues on Firefox 68

--- src/components/cards/skill/index.jsx
+++ src/components/cards/skill/index.jsx
@@ -4,9 +4,12 @@ import ThemeContext from 'state/context/theme';
 import Tooltip from 'components/shared/tooltip';
 import './styles.scss';
 
+const IMG_HEIGHT = 8;
+
 const SkillCard = props => {
   const { icon, iconDark, title, wide, megaWide, ultraWide, height } = props;
   const { theme } = useContext(ThemeContext);
+  const imgHeight = height ? height / 100 * IMG_HEIGHT : IMG_HEIGHT;
   const skillIcon = theme === "d-theme" && iconDark ? iconDark : icon;
   const classes = clsx(
     "SkillCard-container",
@@ -20,7 +23,7 @@ const SkillCard = props => {
         className="skill-img"
         alt={title}
         src={skillIcon}
-        style={{ height: `${height}%` }} 
+        style={{ height: `${imgHeight}rem` }} 
       />
       <Tooltip text={title} />
     </div>
