2137528e92d252d30b38b1f68a9e209c698467d7
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-23 22:41:36 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,10 @@ export $(xargs < ./.env)
 deploy-aws-s3-cloudfront \
   --acl private \
   --bucket apphost-7 \
-  --cache-control index.html:no-cache \
+  --cache-control "/index.html:no-cache" "**/*.svg:max-age=31536000" "**:max-age=604800" \
   --delete \
   --source ./build \
   --destination arkadyt \
-  --distribution E31KW26N7Q3LX8 \
   --invalidation-path "/*" \
+  --distribution E31KW26N7Q3LX8 \
   --non-interactive
-
