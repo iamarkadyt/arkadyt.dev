5a35046e6b22964b4e55c377dcbf3bcdd5baa987
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-24 20:42:43 -0700

    Set no cache on everything for now

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,12 @@ export $(xargs < ./.env)
 deploy-aws-s3-cloudfront \
   --acl private \
   --bucket apphost-7 \
-  --cache-control "index.html:no-cache" "**.svg:max-age=31536000" "**:max-age=604800" \
+  --cache-control "**:no-cache" \
   --delete \
   --source ./build \
   --destination arkadyt \
   --invalidation-path "/*" \
   --distribution E31KW26N7Q3LX8 \
   --non-interactive
+
+# --cache-control "index.html:no-cache" "**.svg:max-age=31536000" "**:max-age=604800" \
