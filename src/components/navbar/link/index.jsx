29aad3137adb34482eb4a585706533db71b64c12
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-25 18:01:51 -0700

    Style buttons on short nav

--- src/components/navbar/link/index.jsx
+++ src/components/navbar/link/index.jsx
@@ -4,11 +4,11 @@ import clsx from 'clsx';
 import './styles.scss';
 
 const NavLink = props => {
-  const { href, ico, title } = props;
+  const { href, ico, title, notext } = props;
   const { theme } = useContext(ThemeContext);
 
   return (
-    <div className={clsx("NavLink-container", theme)}>
+    <div className={clsx("NavLink-container", theme, notext && "notext")}>
       <a href={href} target="_blank" rel="noopener noreferrer">
         <span className="link-icon">{ico}</span>
         <span className="link-text">{title}</span>
