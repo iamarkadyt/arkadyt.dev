1ca475edca5bf9675b4d3f9b98f80a5fd3086e56
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-25 17:54:26 -0700

    Pass new overlap param to navlinks

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import ThemeContext from 'state/context/theme';
 import { FaMoon } from 'react-icons/fa';
 import { IoMdSunny } from 'react-icons/io';
@@ -8,15 +8,30 @@ import clsx from 'clsx';
 import { links } from 'content';
 import './styles.scss';
 
+/**
+ * Point at which nav links start overlapping with nav buttons.
+ * Depends on content length of both.
+ */
+const OVERLAP_POINT = "(max-height: 42rem)";
+
 const NavBar = props => {
   const { theme, toggleTheme } = useContext(ThemeContext);
   const classes = clsx('NavBar-container', theme);
+  const [ isWindowShort, setIsShort ] = useState(
+    window.matchMedia(OVERLAP_POINT).matches
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia(OVERLAP_POINT)
+      .addListener(e => setIsShort(e.matches));
+  }, []);
 
   return (
     <div className={classes}>
       <ul className="sidebar-links">
         {Object.values(links).map((item, index) => (
-          <NavLink key={index} {...item} />
+          <NavLink key={index} {...item} notext={isWindowShort} />
         ))}
       </ul>
       <ul className="sidebar-buttons">
