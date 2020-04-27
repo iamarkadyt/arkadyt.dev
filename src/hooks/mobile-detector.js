1ffd261e9d30dda74dd57f4a798dd7262c989bd2
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-27 16:44:06 -0700

    Initialize mobile detector on cDM instead

--- src/hooks/mobile-detector.js
+++ src/hooks/mobile-detector.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 
 export const useMobileDetector = () => {
-  const mQ = window.matchMedia("(orientation: portrait)");
-  const [ isMobile, setMobile ] = useState(mQ.matches);
+  const [ isMobile, setMobile ] = useState(null);
 
   useEffect(() => {
+    const mQ = window.matchMedia("(orientation: portrait)");
+    setMobile(mQ.matches);
+
     const handleChange = e => setMobile(e.matches);
     mQ.addListener(handleChange);
     return () => mQ.removeListener(handleChange);
