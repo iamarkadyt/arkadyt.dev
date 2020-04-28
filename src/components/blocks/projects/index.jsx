eeccd65f4a198b92ac662584c4bacb62cf26acfa
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-28 16:14:36 -0700

    No hor. projcards on mobile

--- src/components/blocks/projects/index.jsx
+++ src/components/blocks/projects/index.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Section from 'components/shared/section';
 import ProjCard from 'components/cards/project';
 import BigButton from 'components/shared/big-button';
+import { useMobileDetector } from 'hooks';
 import { projects, links } from 'content';
 import clsx from 'clsx';
 import './styles.scss';
+import './styles.mobile.scss';
 
 const Projects = props => {
   const isProjCountOdd = projects.length % 2 > 0;
+  const isMobile = useMobileDetector();
+  const desktopView = (
+      <Fragment>
+          <div className={clsx("projcard-view")}>
+              {(isProjCountOdd ? projects.slice(0, -1) : projects).map((item, index) => (
+                  <ProjCard {...item} key={index} />
+              ))}
+          </div>
+          <div className={clsx("projcard-view", "horizontal")}>
+              {/* 
+                  If project card count is not odd, this div must still render,
+                  but must make no visual effects on the page, and should have no padding and/or margins.
+                  A ghost!
+              */}
+              {!isProjCountOdd ? null : projects.slice(-1).map((item, index) => (
+                  <ProjCard {...item} key={index} horizontal />
+              ))}
+          </div>
+      </Fragment>
+  );
+  const mobileView = (
+      <div className={clsx("projcard-view")}>
+          {projects.map((item, index) => (
+              <ProjCard {...item} key={index} />
+          ))}
+      </div>
+  );
 
   return (
     <Section id="personal-projects" title="Projects">
@@ -21,21 +50,7 @@ const Projects = props => {
                 Majority of them is hosted on my GitHub profile, so if you're interested, click on the "VIEW MORE PROJECTS" button below!
             </p>
         </div>
-        <div className={clsx("projcard-view")}>
-            {(isProjCountOdd ? projects.slice(0, -1) : projects).map((item, index) => (
-                <ProjCard {...item} key={index} />
-            ))}
-        </div>
-        <div className={clsx("projcard-view", "horizontal")}>
-            {/* 
-                If project card count is not odd, this div must still render,
-                but must make no visual effects on the page, and should have no padding and/or margins.
-                A ghost!
-            */}
-            {!isProjCountOdd ? null : projects.slice(-1).map((item, index) => (
-                <ProjCard {...item} key={index} horizontal />
-            ))}
-        </div>
+        {isMobile ? mobileView : desktopView}
         <div className="view-more-row">
             <BigButton title="View more projects" href={links.github.href} showShadow noExplosion />
         </div>
