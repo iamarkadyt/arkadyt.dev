4ba9794801c83f5ce8cdb2d68f2f7fbeea1f8052
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-29 22:59:18 -0700

    Adapt footer for mobile

--- src/components/blocks/contact/index.jsx
+++ src/components/blocks/contact/index.jsx
@@ -3,22 +3,23 @@ import Section from 'components/shared/section';
 import Link from 'components/shared/link';
 import { links } from 'content';
 import './styles.scss';
+import './styles.mobile.scss';
 
 const Contact = props => {
-  return (
-    <Section id="contact" title="Contact" accent {...props}>
-        <div className="contact-view">
-            {Object.values(links).map((item, index) => {
-                const data = {
-                    href: item.href,
-                    title: item.readableLink,
-                    ico: item.ico
-                }
-                return <Link {...data} key={index} />
-            })}
-        </div>
-    </Section>
-  );
+    return (
+        <Section id="contact" title="Contact" accent {...props}>
+            <div className="contact-view">
+                {Object.values(links).map((item, index) => {
+                    const data = {
+                        href: item.href,
+                        title: item.readableLink,
+                        ico: item.ico
+                    }
+                    return <Link {...data} key={index} />
+                })}
+            </div>
+        </Section>
+    );
 };
 
 export default Contact;
